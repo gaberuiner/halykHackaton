752fed1ae45cf89fc2a5484bd06f7c52a7bd1788
godofthesenewworld <godofthesenewworld@gmail.com>
2024-01-27 14:07:45 +0600

    initialize

---
+++ halyk/src/main/java/com/example/halyk/HalykApplication.java
@@ -0,0 +1,13 @@
+package com.example.halyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalykApplication.class, args);
+    }
+
+}
---
+++ halyk/src/test/java/com/example/halyk/HalykApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.halyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalykApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
